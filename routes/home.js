a1796b166bf2be3a1be65a972a1b421fec048865
kristjem <kristjem@pm.me>
2025-01-06 17:35:31 +0100

    Refactor typed.js code. Add nodedemon

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs")
+const path = require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Home' });
+  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  res.render('home', { title: 'Home', array: JSON.parse(data)});
 });
 
 module.exports = router;

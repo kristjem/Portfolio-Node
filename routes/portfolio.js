dd12f19ada1ba3d9e1dead7363ff40c90a5d84f8
kristjem <kristjem@pm.me>
2025-01-07 16:10:16 +0100

    Add DELETE handler for portfolio

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -26,6 +26,20 @@ router.post('/', jsonParser, function(req, res, next) {
   res.end();
 });
 
+/* DELETE portfolio page. */
+router.delete('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  let portfoliosArray = JSON.parse(rawdata);
+  const newArray = portfoliosArray.filter(x => x.name !== req.body.name)
+  if(newArray.length !== portfoliosArray.length) {
+    fs.unlink(path.resolve(__dirname, '../data/img/'+ req.body.name), () => {
+      console.log(req.body.name + " deleted!");
+    });
+    fs.writeFileSync(path.resolve(__dirname, "../data/portfolio.json"), JSON.stringify(newArray, null, 2));
+  }
+  res.end();
+});
+
 var download = function(url, filename, callback){
   request.head(url, function(err, res, body){
     request(url).pipe(fs.createWriteStream(path.resolve(__dirname, '../data/img/'+ filename))).on('close', callback);

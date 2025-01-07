e42413406907529ec0cdeb1300ac7d18074f186e
kristjem <kristjem@pm.me>
2025-01-07 10:03:00 +0100

    Add handling deleted introductions

--- routes/home.js
+++ routes/home.js
@@ -21,4 +21,15 @@ router.post('/', jsonParser, function(req, res, next) {
   res.end();
 });
 
+router.delete('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  let array = JSON.parse(rawdata);
+  // The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+  // No "for...of" loop is needed, because the filter() method does the same thing using a callback function for each element.
+  const newArray = array.filter(item => item !== req.body.deletedText);
+
+  fs.writeFileSync(path.resolve(__dirname, "../data/introductionArray.json"), JSON.stringify(newArray));
+  res.end();
+});
+
 module.exports = router;

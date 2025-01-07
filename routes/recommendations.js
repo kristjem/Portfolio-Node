1088b4711b4809bc2a0b0a70116087e9de8258ed
kristjem <kristjem@pm.me>
2025-01-07 15:02:36 +0100

    Add POST and DELETE recommendations handlers

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -2,12 +2,35 @@ var express = require('express');
 var router = express.Router();
 const fs = require("fs")
 const path = require("path")
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let data = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
   res.render('recommendations', { title: 'Recommendations', data: JSON.parse(data)
  });
+});
+
+router.post('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let recommendationsArray = JSON.parse(rawdata);
+  if(recommendationsArray.filter(x => x.name === req.body.name).length == 0) {
+    const newArray = recommendationsArray.concat([req.body])
+    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray, null, 2));
+  }
+  res.end();
+});
+
+router.delete('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let recommendationsArray = JSON.parse(rawdata);
+  // In effect deleting the recommendation with the name that matches the name in the request body
+  const newArray = recommendationsArray.filter(x => x.name !== req.body.name)
+  if (newArray.length !== recommendationsArray.length ) {
+    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray, null, 2));
+  }
+  res.end();
 });
 
 module.exports = router;
